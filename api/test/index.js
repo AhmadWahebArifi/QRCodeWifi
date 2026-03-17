bafa0181c6beecd0af8d858f04ae2252bc6326af
Ahmad Arifi <ahmadwahebarifi@gmail.com>
2026-03-17 14:21:11 +0430

    index

---
+++ api/test/index.js
@@ -0,0 +1,18 @@
+export default async function handler(request) {
+  return new Response(
+    JSON.stringify({
+      message: "API is working",
+      timestamp: new Date().toISOString(),
+      method: request.method,
+    }),
+    {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type",
+      },
+    }
+  );
+}
